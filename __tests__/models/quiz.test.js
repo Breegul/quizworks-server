3748dec1f0a9211f88bc02a7791d1a9b5aea2f0a
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-06 19:02:21 +0000

    Create __tests__/models/quiz.test.js

---
+++ __tests__/models/quiz.test.js
@@ -0,0 +1,39 @@
+const Quiz = require('../../models/quiz');
+
+describe('Quiz model', () => {
+    let quiz;
+
+    beforeAll(() => {
+        quiz = new Quiz({ quiz_id: 1, title: 'Math Quiz', description: 'A quiz about math', user_id: 1 });
+    });
+
+    describe('getAllQuizzes', () => {
+        test('is a function', () => {
+            expect(typeof quiz.getAllQuizzes).toBe('function');
+        });
+    });
+
+    describe('getAllQuizzesByUserId', () => {
+        test('is a function', () => {
+            expect(typeof quiz.getAllQuizzesByUserId).toBe('function');
+        });
+    });
+
+    describe('getOneQuizById', () => {
+        test('is a function', () => {
+            expect(typeof quiz.getOneQuizById).toBe('function');
+        });
+    });
+
+    describe('updateQuizById', () => {
+        test('is a function', () => {
+            expect(typeof quiz.updateQuizById).toBe('function');
+        });
+    });
+
+    describe('deleteQuizById', () => {
+        test('is a function', () => {
+            expect(typeof quiz.deleteQuizById).toBe('function');
+        });
+    });
+});
