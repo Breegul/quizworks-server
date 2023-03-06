6086a100530ba235451fd1401189e144d98c2285
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-06 10:37:02 +0000

    Update server.js

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ server.get('/', (req, res) => {
 });
 
 // Routes
-server.use('/users', userRoutes);
-server.use('/quizzes', tokenController.authenticateUser, quizRoutes);
-server.use('/notes', tokenController.authenticateUser, noteRoutes);
+//server.use('/users', userRoutes);
+//server.use('/quizzes', tokenController.authenticateUser, quizRoutes);
+//server.use('/notes', tokenController.authenticateUser, noteRoutes);
 
 //Error handling middleware
 server.use((err, req, res, next) => {
