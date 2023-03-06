0870aa789d20f70b4cd8ddcc6e066f7f2b25d31d
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-06 19:02:10 +0000

    Create __tests__/models/note.test.js

---
+++ __tests__/models/note.test.js
@@ -0,0 +1,39 @@
+const Note = require('../../models/note');
+
+describe('Note model', () => {
+    let note;
+
+    beforeAll(() => {
+        note = new Note();
+    });
+
+    describe('getNoteById', () => {
+        test('is a function', () => {
+            expect(typeof note.getNoteById).toBe('function');
+        });
+    });
+
+    describe('getAllByUserId', () => {
+        test('is a function', () => {
+            expect(typeof note.getAllByUserId).toBe('function');
+        });
+    });
+
+    describe('createNote', () => {
+        test('is a function', () => {
+            expect(typeof note.createNote).toBe('function');
+        });
+    });
+
+    describe('updateNoteById', () => {
+        test('is a function', () => {
+            expect(typeof note.updateNoteById).toBe('function');
+        });
+    });
+
+    describe('deleteNoteById', () => {
+        test('is a function', () => {
+            expect(typeof note.deleteNoteById).toBe('function');
+        });
+    });
+});
