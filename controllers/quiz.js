c3b0c4de760ba3888fa0ebebfd07281dd385ac74
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-06 14:51:59 +0000

    Added error handling to all modules and expanded on quiz backend functionality.

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,36 +1,70 @@
 const Quiz = require("../models/quiz");
 
-async function index (req, res) {
+async function getAllQuizzes(req, res) {
     try {
-        const quizzes = await Quiz.getAll();
+        const quizzes = await Quiz.getAllQuizzes();
         res.status(200).json(quizzes);
     } catch (err) {
-        res.status(500).json({"error": err.message});
+        res.status(500).json({ "error": err.message });
     }
 }
 
-async function indexUserId(req, res) {
+async function getAllQuizzesByUserId(req, res) {
     try {
         const user_id = parseInt(req.params.id)
-        const quizzes = await Quiz.getByUserId(user_id);
+        const quizzes = await Quiz.getAllQuizzesByUserId(user_id);
         res.status(200).json(quizzes)
     } catch (err) {
-        res.status(500).json({"error": err.message});
+        res.status(500).json({ "error": err.message });
     }
 }
 
-async function show (req, res) {
+async function getOneQuizById(req, res) {
     try {
         const id = parseInt(req.params.id);
-        const quiz = await Quiz.getById(id);
+        const quiz = await Quiz.getOneQuizById(id);
         res.status(200).json(quiz);
     } catch (err) {
-        res.status(404).json({"error": err.message})
+        res.status(404).json({ "error": err.message }) // check status 404 or 500
+    }
+}
+
+async function updateQuizById(req, res) {
+    try {
+        const quiz = await Quiz.getById(req.params.id);
+        if (!quiz) {
+            return res.status(404).json({ message: 'Quiz not found' });
+        }
+        if (quiz.user_id !== req.user.id) {
+            return res.status(403).json({ message: 'You are not authorized to update this quiz' });
+        }
+        const updatedQuiz = await Quiz.updateQuizById(req.params.id, req.body.title, req.body.description, req.user.id);
+        return res.json(updatedQuiz);
+    } catch (error) {
+        res.status(500).json({ "error": err.message });
+    }
+}
+
+async function deleteQuizById(req, res, next) {
+    try {
+        const quiz = await Quiz.getById(req.params.id);
+        if (!quiz) {
+            return res.status(404).json({ message: 'Quiz not found' });
+        }
+        if (quiz.user_id !== req.user.id) {
+            return res.status(403).json({ message: 'You are not authorized to delete this quiz' });
+        }
+        await Quiz.deleteQuizById(req.params.id);
+        return res.json({ message: 'Quiz deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ "error": err.message });
     }
 }
 
 module.exports = {
-    index,
-    show,
-    indexUserId
+    getAllQuizzes,
+    getAllQuizzesByUserId,
+    getOneQuizById,
+    updateQuizById,
+    deleteQuizById
 }
--- routes/quiz.js
+++ routes/quiz.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const quizController = require('../controllers/quiz');
 const router = Router();
 
-router.get('/', quizController.index);
+router.get('/', quiz.quizController.index);
 router.get('/:id', quizController.show);
 router.get('/user/:id', quizController.indexUserId);
 
