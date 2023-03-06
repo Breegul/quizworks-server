6e128ff68971c95634e62e7a67e0d3fab6087749
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-06 15:07:18 +0000

    Update controllers/token.js - add auth middleware for authentication user and validating token.

--- controllers/token.js
+++ controllers/token.js
@@ -1,5 +1,28 @@
+const Token = require("../models/token");
+const User = require("../models/user");
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
+
+async function authenticateUser(req, res, next) {
+  try {
+      const { token } = req.headers;
+      if (!token) {
+        return res.status(401).json({ message: 'Authentication failed. Token is missing.' });
+      }
+      const tokenHash = crypto.createHash('sha256').update(token).digest('hex');
+      const tokenObject = await Token.getTokenByTokenHash(tokenHash);
+      if (tokenObject.isExpired()) {
+        return res.status(401).json({ message: 'Authentication failed. Token has expired.' });
+      }
+      const user = await User.getUserById(tokenObject.userId);
+      req.user = user;
+      res.status(200).json(user);
+      return next();        
+  } catch (error) {
+      console.error(error);
+      return res.status(401).json({ message: 'Authentication failed.' });
+  }
+}
 
 function authenticateToken(req, res, next) {
   // Get the authorization header from the request
@@ -15,11 +38,14 @@ function authenticateToken(req, res, next) {
     // Verify the token and extract the user ID from it
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = { id: decoded.id };
-    next();
+    return next();
   } catch (err) {
     // If the token is invalid, return a 403 Forbidden response
     return res.status(403).json({ error: 'Forbidden' });
   }
 }
 
-module.exports = { authenticateToken };
+module.exports = { 
+  authenticateToken,
+  authenticateUser
+ };
