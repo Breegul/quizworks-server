13106dfa478da70bad07b276429beebe22e97533
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-06 09:50:58 +0000

    Create db-seeds.js

---
+++ database/db-seeds.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const pool = require('./pool.js');
+
+dotenv.config();
+
+const sqlSeedDatabase = fs.readFileSync(__dirname + '/seeds.sql').toString();
+
+async function seedDatabase() {
+  const client = await pool.connect();
+  try {
+    await client.query(sqlSeedDatabase);
+    console.log('Sample data added successfully.');
+  } catch (err) {
+    console.error('Error adding sample data: ', err);
+  } finally {
+    client.release();
+    client.end();
+  }
+}
+
+seedDatabase();
