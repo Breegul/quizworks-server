2de8c5c65acb271fcebdbf9506fc93a62540875d
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-07 15:44:52 +0000

    Update __tests__/models/note.test.js

--- __tests__/models/note.test.js
+++ __tests__/models/note.test.js
@@ -1,4 +1,5 @@
 const Note = require('../../models/note');
+const { pool } = require('../../database/pool.js');
 
 describe('Note model', () => {
     let note;
@@ -7,14 +8,40 @@ describe('Note model', () => {
         note = new Note();
     });
 
+    afterAll(async () => {
+        await pool.end();
+    });
+
     test('constructor is a function', () => {
         expect(typeof Note).toBe('function');
-      });
+    });
 
     describe('getNoteById', () => {
         test('is a function', () => {
             expect(typeof note.getNoteById).toBe('function');
         });
+        test('should return a note object with valid id', async () => {
+            // Insert a new note into the database
+            const insertQuery = 'INSERT INTO notes (title, content) VALUES ($1, $2) RETURNING *';
+            const insertValues = ['Test Note', 'This is a test note.'];
+            const { rows } = await pool.query(insertQuery, insertValues);
+            const noteId = rows[0].id;
+            // Retrieve the note by id
+            const result = await note.getNoteById(noteId);
+            // Check if the result is a note object
+            expect(result).toBeDefined();
+            expect(result).toHaveProperty('id');
+            expect(result).toHaveProperty('title');
+            expect(result).toHaveProperty('content');
+            expect(result.id).toBe(noteId);
+            expect(result.title).toBe('Test Note');
+            expect(result.content).toBe('This is a test note.');
+          });
+
+          test('should throw an error with invalid id', async () => {
+            // Try to retrieve a non-existent note
+            await expect(note.getNoteById(999)).rejects.toThrow('Unable to locate note.');
+          });
     });
 
     describe('getAllByUserId', () => {
