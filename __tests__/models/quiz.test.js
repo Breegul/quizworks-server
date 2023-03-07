82c865f8f4501d034a49266e20453467d70b1555
GabrielleMonaen <117737870+GabrielleMonaen@users.noreply.github.com>
2023-03-07 17:48:41 +0000

    Update __tests__/models/quiz.test.js

--- __tests__/models/quiz.test.js
+++ __tests__/models/quiz.test.js
@@ -2,11 +2,6 @@ const Quiz = require('../../models/quiz');
 const { pool } = require('../../database/pool.js');
 
 describe('Quiz model', () => {
-    let quiz;
-
-    beforeAll(() => {
-        quiz = new Quiz({});
-    });
 
     afterAll(async () => {
         await pool.end();
@@ -24,15 +19,15 @@ describe('Quiz model', () => {
 
     describe('getAllQuizzesByUserId', () => {
         test('is a function', () => {
-            expect(typeof quiz.getAllQuizzesByUserId).toBe('function');
+            expect(typeof Quiz.prototype.getAllQuizzesByUserId).toBe('function');
         });
     });
 
     describe('getOneQuizById', () => {
         test('is a function', () => {
-            expect(typeof quiz.getOneQuizById).toBe('function');
+            expect(typeof Quiz.getOneQuizById).toBe('function');
         });
-/*
+
         test('should return a quiz object with valid id', async () => {
             // Insert a new quiz into the database
             const insertQuery = 'INSERT INTO quizzes (title, description, user_id) VALUES ($1, $2, $3) RETURNING *';
@@ -41,7 +36,7 @@ describe('Quiz model', () => {
             const quizId = rows[0].id;
 
             // Retrieve the quiz by id
-            const result = await quiz.getOneQuizById(quizId);
+            const result = await Quiz.getOneQuizById(quizId);
 
             // Check if the result is a quiz object
             expect(result).toBeDefined();
@@ -57,20 +52,24 @@ describe('Quiz model', () => {
 
         test('should throw an error with invalid id', async () => {
             // Try to retrieve a non-existent quiz
-            await expect(quiz.getOneQuizById(999)).rejects.toThrow('Unable to locate quiz.');
+            await expect(Quiz.getOneQuizById(999)).rejects.toThrow('Unable to locate quiz.');
         }); 
-        */
+        
     });
-
+    describe('create', () => {
+        test('is a function', () => {
+            expect(typeof Quiz.create).toBe('function');
+        });
+    });
     describe('updateQuizById', () => {
         test('is a function', () => {
-            expect(typeof quiz.updateQuizById).toBe('function');
+            expect(typeof Quiz.prototype.updateQuizById).toBe('function');
         });
     });
 
     describe('deleteQuizById', () => {
         test('is a function', () => {
-            expect(typeof quiz.deleteQuizById).toBe('function');
+            expect(typeof Quiz.prototype.deleteQuizById).toBe('function');
         });
     });
 });
