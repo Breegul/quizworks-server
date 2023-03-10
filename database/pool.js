e2ad0d3772f2d9337ed98a152abfe1a0479fc01a
Martin <31014184+Breegul@users.noreply.github.com>
2023-03-10 03:47:36 +0000

    Update pool.js

--- database/pool.js
+++ database/pool.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DB_URL/*,
   ssl: {
     rejectUnauthorized: false
-  }
+  }*/
 });
 
 //module.exports = pool;
